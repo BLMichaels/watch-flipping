0cc756f6e94abdda0e37ac189ca59095b5d9988b
BLMichaels <155132733+BLMichaels@users.noreply.github.com>
2025-12-24 01:23:17 -0500

    Fix ebay-scraper: Remove all cheerio references completely

---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+export default function TestPage() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>✅ Test Page - Build Successful!</h1>
+      <p>If you can see this, the app is working.</p>
+    </div>
+  );
+}
+
